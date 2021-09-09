5bdfd63d31bb7a03bc62ab08be01ec07d8adad76
aashish-cd <076bct002@ioepc.edu.np>
2021-09-09 14:10:47 +0545

    next

--- pages/page/index.js
+++ pages/page/index.js
@@ -8,7 +8,15 @@ const Home = () => {
       <Container className='bg-dark p-4'>
         <Container maxWidth='md'>
           <div>
-            <Typography variant='h1' align='center' color='white' gutterBottom>
+            <Typography
+              variant='h1'
+              align='center'
+              color='white'
+              style={{
+                fontSize: '3rem',
+              }}
+              gutterBottom
+            >
               {profileData[0].title}
             </Typography>
             <Typography variant='h5' align='center' color='white' paragraph>
